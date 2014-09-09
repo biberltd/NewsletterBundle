1f5b16d3c14d781bda9b02a4e98fb2573123657c
Said İmamoğlu <said.imamoglu@biberltd.com>
2014-09-09 17:13:10 +0300

    Namespaces upgraded to newer version.

---
+++ Resources/translations/newsletter.tr.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * newsletter.tr.php
+ *
+ * This file registers the bundle specific default translations.
+ *
+ * @vendor      BiberLtd
+ * @package		Core\Bundles\MemberManagementBundle
+ * @subpackage	Resources
+ * @name	    newsletter.tr.php
+ *
+ * @author		Can Berkol
+ *
+ * @copyright   Biber Ltd. (www.biberltd.com)
+ *
+ * @version     1.0.0
+ * @date        02.01.2014
+ *
+ * =============================================================================================================
+ * !!! IMPORTANT !!!
+ *
+ * Depending your environment run the following code after you have modified this file to clear Symfony Cache.
+ * Otherwise your changes will NOT take affect!
+ *
+ * $ sudo -u apache php app/console cache:clear
+ * OR
+ * $ php app/console cache:clear
+ * =============================================================================================================
+ * TODOs:
+ * None
+ */
+return array(
+    'phrase'    => array(
+        'browser'           => 'tarayıcınızda görüntüleyin.',
+        'unsubscribe'       => 'Bu listedeki üyeliğinizi iptal edin',
+        'update'            => 'Üyeliğinizi güncelleyin.',
+        'view_on'           => 'Bu mesajı',
+        'view_on_browser'   => 'Bu mesajı tarayıcınızda görüntüleyin',
+    ),
+);
+/**
+ * Change Log
+ * **************************************
+ * v1.0.0                      Can Berkol
+ * 02.01.2014
+ * **************************************
+ * A phrase
+ * A phrase.browser
+ * A phrase.unsubscribe
+ * A phrase.view_on
+ * A phrase.view_on_browser
+ *
+ */
